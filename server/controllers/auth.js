031773cf6b1dae81f47834718522405056987baf
HDRanasingha <138963111+HDRanasingha@users.noreply.github.com>
2024-08-16 15:31:21 +0530

    Added files to repository

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,49 +1,57 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import User from '../models/user.js';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-/*REGISTER USER*/
+/* REGISTER USER */
 export const register = async (req, res) => {
-    try {
-        const { firstname, lastname, email, password,picturePath,friends,location,occupation } = req.body;
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(password, salt);
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
 
-        const newUser = new User({
-            firstname,
-            lastname,
-            email,
-            password: hashedPassword,
-            picturePath,
-            friends,
-            location,
-            occupation,
-            viewedProfile:Math.floor(Math.random() * 10000),
-            impressions:Math.floor(Math.random() * 10000),
-        });
-        const saveduser = await newUser.save();
-        res.status(201).json(saveduser);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
 
-    }}
-    /*LOGGING IN */
-    export const login =async (req,res)=>{
-        try {
-            const {email,password} = req.body;
-            const user = await User.findOne({email:email});
-            if(!user)
-                return res.status(400).json({msg:"User does not exist"});
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
-            const isMatch = await bcrypt.compare(password,user.password);
-            if(!isMatch)
-                return res.status(400).json({msg:"Invalid credentials"});
+/* LOGGING IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
-            const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
-            delete user.password;
-            res.status(200).json({token,user});
-            
-    }
-    catch (error) {
-        res.status(500).json({ message: error.message });
-    }}
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
